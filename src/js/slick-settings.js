de5daddc9eb0dd3eb33ef318ecf2ffc01c01e2f1
Andrey Katrych <andrew.katrich@gmail.com>
2021-07-08 00:46:42 +0300

    feature/read-more
    Добвляет опцию Read More в секцию reviews
    Подключает файл стилей _read-more.scss

--- src/js/slick-settings.js
+++ src/js/slick-settings.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $('.slider').slick({
     arrows: true,
     dots: true,
-    adaptiveHeight: true,
     easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
     initialSlide: 5,
     autoplay: true,
